c6947993db0af708756a22c9da4836f7c7ac9a86
n-batrakov <n.m.batrakov@gmail.com>
2019-11-12 15:59:08 +0300

    Fix request parsing

--- src/santa/parse.ts
+++ src/santa/parse.ts
@@ -4,13 +4,11 @@ import { SlashCommandRequest } from './types'
 export function parseRequest(request: MattermostSlashCommandRequest): SlashCommandRequest {
     const [command, ...rest] = request.text.split(' ')
 
-    console.log('REQUEST', request)
-
     return {
         request,
         command,
         text: rest.join(' '),
         username: request.user_name,
-        channel: request.channel_id,
+        channel: request.channel_name,
     }
 }
